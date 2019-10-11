439aed928007d137f244bd18fc6ddeddb4e3fcb6
Jeremy Collins <jeremyccollins@gmail.com>
2019-10-11 15:41:36 +0800

    added form to api

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ RUN pip install --upgrade pip
 RUN pip install -U python-docx pymongo virtualenv textrazor
 RUN apt-get update
 RUN apt-get install -y mongodb-clients
-RUN pip install flask flask_restful
+RUN pip install flask flask_restful wtforms
 #RUN virtualenv /keras
 #RUN /keras/bin/pip install tensorflow==2.0.0-rc0 sklearn pandas
--- api.py
+++ api.py
@@ -3,7 +3,12 @@ from flask_restful import Resource, Api
 from load_data_pipeline import load
 from search import search_by_id
 
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 api = Api(app)
 
 directory_name = 'Files'
@@ -17,6 +22,21 @@ class Search(Resource):
    def get(self, search_id):
      search_id = search_id.capitalize()
      return str(search_by_id(search_id))
+
+class ReusableForm(Form):
+    name = TextField('Search term:', validators=[validators.required()])
+    
+    @app.route("/search", methods=['GET', 'POST'])
+    def hello():
+        form = ReusableForm(request.form)
+    
+        print(form.errors)
+        if request.method == 'POST':
+            name=request.form['name']
+            search_id = name.capitalize()
+            return str(search_by_id(search_id))
+        return render_template('form.html', form=form)
+
 
 api.add_resource(Load, '/load')
 api.add_resource(Search, '/search/<string:search_id>')
