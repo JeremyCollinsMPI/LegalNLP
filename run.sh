cf3b73653822fa17661566a09c9196476f211123
Jeremy Collins <jeremyccollins@gmail.com>
2019-10-14 17:04:11 +0800

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 name='mongo'
 [[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] ||
-docker run -d -p 27017:27017 -v ~/data:/data/db --name mongo mongo
+docker run --rm -d -p 27017:27017 -v ~/data:/data/db --name mongo mongo
 name='stanford'
 [[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] ||
-docker run -p 9000:9000 --name stanford nlpbox/corenlp
+docker run -d --rm -p 9000:9000 --name stanford nlpbox/corenlp
 docker run -it --rm -e mongo_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongo) -e stanford_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' stanford) -v $PWD:/directory -p 5000:5000 jeremycollinsmpi/legalnlp:light /bin/bash
 
 # inside container
