55d409406be5e264dcc2bd300f55c151bf356695
Jeremy Collins <jeremyccollins@gmail.com>
2019-10-14 20:18:12 +0800

    sample query on a text

--- sample_query.py
+++ sample_query.py
@@ -5,8 +5,12 @@ stanford_ip = os.environ["stanford_ip"]
 
 url='http://172.17.0.3:9000/?properties={"annotators":"parse","outputFormat":"json"}'
 
-sentence = 'I am a moose'
+# sentence = 'I am a moose.  You are a dog'
+sentence = open('judgements/texts/1.txt')
 
 response = requests.post(url, sentence)
 
-print(response.json())
+
+dict = response.json()
+print(len(dict['sentences']))
+print(dict['sentences'][20])
